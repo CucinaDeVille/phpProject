50b7162ba190a6ff06c526fbd97fece0c6c4f6bd
luca <luca.romano@t-online.de>
2026-04-23 12:49:18 +0200

    feat(mySQL): :sparkles: query user data on homepage

--- htdocs/home.php
+++ htdocs/home.php
@@ -20,6 +20,34 @@
 		?>
 		</h1>
 
+		<?php
+			$mysqli = new mysqli("db", "user", "user", "testdb");
+  			if ($mysqli->connect_errno) {
+    			die("Verbindung fehlgeschlagen: " . $mysqli->connect_error);
+  			}
+
+  			$sql = "SELECT * FROM users;";
+  			$statement = $mysqli->prepare($sql);
+  			$statement->execute();
+
+  			$result = $statement-> get_result();
+
+			print("<table style='border:5px solid red;'>");
+
+			while($row = $result->fetch_object()) {
+				print("<tr>");
+				print("<td>");
+  				print ($row->Benutzername);
+				print("</td>");
+				print("<td>");
+				print ($row->passwort);
+				print("</td>");
+				print("</tr>");
+			}
+
+			print("</table>");
+		?>
+
 		<form action="logout.php" method="post">
 			<button type="submit">Logout</button>
 		</form>
