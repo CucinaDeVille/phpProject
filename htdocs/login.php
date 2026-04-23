f870b93846a3f3687060cf9ef198ee2e257bc921
luca <luca.romano@t-online.de>
2026-04-23 12:04:50 +0200

    feat(mySQL): :sparkles: add db for login

--- htdocs/login.php
+++ htdocs/login.php
@@ -1,14 +1,23 @@
 <?php 
 
-  
   if (isset($_GET["username"])) {
 	$username = $_GET["username"];
   }
   if (isset($_GET["password"])) {
 	$password = $_GET["password"];
   }
+
+  $mysqli = new mysqli("db", "user", "user", "testdb");
+  if ($mysqli->connect_errno) {
+    die("Verbindung fehlgeschlagen: " . $mysqli->connect_error);
+  }
+
+  # ' OR 1=1 limit 1#
+  $sql = "SELECT * FROM users WHERE benutzername='$username' AND passwort='$password';";
+  $result = $mysqli->query($sql);
+  $anzahl_treffer = $result->num_rows;
   
-  if ($username=="steffen" && $password=="abc") {
+  if ($anzahl_treffer == 1) {
 	  header("location: home.php");
 	  session_start();
 	  $_SESSION["username"]=$username;
@@ -16,5 +25,4 @@
 	  header("location: login.html");
   }
  
-
 ?>
